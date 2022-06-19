df47f8f12771a51f66709a5271d408cccf24142c
Corazon07 <wangsonam23@gmail.com>
2022-06-19 17:24:38 +0530

    final changes

--- SMS/app/Http/Controllers/SendSMSController.php
+++ SMS/app/Http/Controllers/SendSMSController.php
@@ -46,9 +46,7 @@ class SendSMSController extends Controller
         $auth_token = env("TWILIO_AUTH_TOKEN",'6a8074281724262385c3fba6dfce2007');
         $twilio_number = env("TWILIO_NUMBER",'+15005550006');
 
-        // $account_sid = getenv("TWILIO_SID");
-        // $auth_token = getenv("TWILIO_AUTH_TOKEN");
-        // $twilio_number = getenv("TWILIO_NUMBER");
+       
         $allnumbers=$request->mobile_number;
         $numbers = explode(',', $allnumbers);
         // dd($account_sid);
@@ -56,25 +54,10 @@ class SendSMSController extends Controller
         foreach($numbers as $number) {
             $mobile="+91".$number;
 
-            // $send_request = $client->request("POST",$url,[
-                // 'form_params' => [
-                    // 'from' => $twilio_number, 'body' => 'Test assignment'  ,'to'=>[$mobile],
-                // ]
-            // ]);
-            // $response = $send_request->getBody()->getContents();
-            // $http = new Services_Twilio_TinyHttp(
-            //     'https://api.twilio.com',
-            //     array('curlopts' => array(
-            //         CURLOPT_SSL_VERIFYPEER => true,
-            //         CURLOPT_SSL_VERIFYHOST => 2,
-            //     ))
-            // );
-            
-            // $client = new Services_Twilio($sid, $token, "2010-04-01", $http);
-            
+      
             $client2= $client->messages->create($mobile, 
             ['from' => $twilio_number, 'body' => 'Test assignment'] );
-            dd($client2);
+            // dd($client2);
           }
         // dd($numbers);
         return redirect()->back()->with('message', 'Message Delivered Successfully');
